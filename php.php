234ba7ea016c9be28e18ac49e5b968f8592ca854
Wodru <toni.am.martinez@gmail.com>
2018-02-02 13:22:28 +0100

    Crear enlace style añadir style.css prueba fichero php

---
+++ php.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>página hecha con php</title>
+  </head>
+  <body>
+    <p><?php echo 'Esto es un echo de php'; ?></p>
+  </body>
+</html>
